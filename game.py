dc5b9d5d67b730619dd5bc0e770a2a9c4913dfd7
tc5368 <tc536890@gmail.com>
2018-10-21 05:30:47 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -2,8 +2,9 @@
 from rooms import *
 from items import *
 current_room = elevator_corridor
+global inventory
 
-
+inventory = [pen,computer]
 
 def main():
 	global current_room
@@ -27,6 +28,18 @@ def move(direction):
 	global current_room
 	current_room = current_room.go(direction)
 
+def take_item(item):
+	if current_room.remove_item(item):
+		print('You have succsessfully picked up,',item.get_name())
+		inventory.append(item)
+	else:
+		print('You can\'t take that')
+
+def drop_item(item):
+	if item in inventory:
+		print('You have dropped',item.get_name())
+		del(inventory[inventory.index(item)])
+		current_room.add_item(item)
 
 def is_game_still_going():
 	return True
